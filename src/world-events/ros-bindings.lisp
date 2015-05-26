6eb50256ce1fb97862cbf16493e040fc7def773c
Kunal Tyagi <tyagi.kunal@live.com>
2015-05-26 18:36:30 +0200

    service completed, sort of

--- src/world-events/ros-bindings.lisp
+++ src/world-events/ros-bindings.lisp
@@ -10,8 +10,7 @@
   "Subscribes to topics, binds call backs"
   (setf *add-event-sub* (subscribe "add_event" ros-binding-name #'add-event-cb))
   (setf *raise-event-pub* (advertise "new_event" ros-binding-name #'raise-event-pb))
-  ; action, service server and client
-
+  (register-service "event_status" 'GetEventStatus)
 )
 
 ;; @TODO: save details of msg, and add the created Event to the list *world-event-accessor-list*
@@ -20,6 +19,17 @@
 (defun raise-event-pb (msg) "Publishes already prepared messages"
   (publish *raise-event-pub* msg))
 
-;; @TODO: complete this function
+;; @TODO: add time-stamp here
 (defmethod prepare-msg ((event world-event))
-  t)
+  (make-message "event_bullet_world/EventUpdate"
+                :header (make-msg "std_msgs/Header"
+                                  :stamp 2342)
+                :name (event-name event)
+                :status (raise-event-on-true event)))
+
+;; @TODO: add time-stamp here
+(def-service-callback GetEventStatus (name)
+  (make-response "event_bullet_world/EventStatus"
+                 :header (make-msg "std_msgs/Header"
+                                   :stamp 2342)
+                 :status (raise-event-on-true event)))
