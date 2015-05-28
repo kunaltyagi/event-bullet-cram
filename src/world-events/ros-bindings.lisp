96219a321bab58af71d1d2d1d6a67e2092b66c4e
Kunal Tyagi <tyagi.kunal@live.com>
2015-05-28 10:59:57 +0530

    For creation of the lambda fn for comparision

--- src/world-events/ros-bindings.lisp
+++ src/world-events/ros-bindings.lisp
@@ -15,6 +15,30 @@
 
 ;; @TODO: save details of msg, and add the created Event to the list *world-event-accessor-list*
 (defun add-event-cb (msg) "Callback for new event values" (setf (value *add-event-msg*) msg))
+;; @TODO: defun position (obj_name), velocity (obj_name), acceleration (obj_name)
+;; if empty, take obj_name to be "world" instead
+
+;; Slight renaming required. Target object means relative wrt the target object, not wrt the source object
+
+;; @TODO: remove is_range, not using it
+;; @TODO: use is_magnitude, overload < and > for use
+;; (let (value (get-current-value msg))
+;;   (if is_interior (and (> min value) (< max value)) (or (< min value) (> max value))))
+
+;; (defun get-current-value(msg) "Returns the required relatice/absolute value based on target and current objects as well as the constraint_type"
+;;   (cond ((= constraint_type POSITION)
+;;           (if is_relative (- (position target_object) (position source_object)) (position source_object))
+;;         )
+;;         ((= constraint_type VELOCITY)
+;;           (if is_relative (- (velocity target_object) (velocity source_object)) (velocity source_object))
+;;         )
+;;         ((= constraint_type ACCELERATION)
+;;           (if is_relative (- (acceleration target_object) (acceleration source_object)) (acceleration source_object))
+;;         )
+;;         (t 
+;;           (ros-warn EVENT_BULLET_WORLD "Wrong constraint_type provided, falling back to position")
+;;           (if is_relative (- (position target_object) (position source_object)) (position source_object)) ;; or call the fn again with constraint_type as POSITION? which is better?
+;;         ))
 
 (defun raise-event-pb (msg) "Publishes already prepared messages"
   (publish *raise-event-pub* msg))
