71f76f7ea6da30ff1ce759b4bd34271508e0f88f
Kunal Tyagi <tyagi.kunal@live.com>
2015-06-03 21:08:29 +0200

    brain wave

--- src/world-event/node.lisp
+++ src/world-event/node.lisp
@@ -8,12 +8,12 @@
     (ros-warn WORLD-EVENT-CLASS "Event raising node started")
     (loop-at-most-every loop-rate
       ;; @TODO: Create threads for each object? mskes sense if the number of objects becomes large
-      ;; How will this work?
-      ;; Option 1: for each object create a thread which runs in a
-      ;; seperate loop-at-most-every, then, removing an object from the list has to
-      ;; be handled
-      ;; Option 2: current model: check each object parallely, problem: too many
-      ;; thread created and destroyed every cycle.
+      ;; after adding an event, create a thread with loop-rate
+      ;; on asking for deletion, destroy the thread.
+      ;; Maintain a seperate list of objects to be destroyed.
+      ;; In the thread, look for the event in the to-be-destroyed list, if present,
+      ;; exit thread after calling remove fn for that object.
+      ;; PS: maintain read-write lock
       (loop for event in *world-event-accessor-list* when (funcall (raise-event-on-true event))
         do (on-event event)))))
 
